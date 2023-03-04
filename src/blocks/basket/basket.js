a090102f7780a4cf254bae61da163a82d90c364d
Aslan Abaev <hedgehog5349@gmail.com>
2023-03-04 17:26:07 +0300

    dev basket

--- src/blocks/basket/basket.js
+++ src/blocks/basket/basket.js
@@ -1,66 +1,188 @@
-o2.basket = {
-	// state:{
-		// sum: 0,
-		// count: 0,
-	// },
+// o2.basket = {
+// 	sumContainer: false,
+// 	countContainer: false,
 
-	plus() {
-		const counter = event.target.closest('._buttons').querySelector('._counter');
-		let counterContent = event.target.closest('._buttons').querySelector('._counter').textContent;
-		let count = Number(counterContent) + 1;
-		counter.innerText = count;
-		this.counters();
+// 	state:{
+// 		sum: 0,
+// 		count: 0,
+// 		items: {
+// 			1111: {
+// 				sum: 0,
+// 				count: 0
+// 			}
+// 		},
+// 	},
 
-		let price = event.target.closest('.basket__item').querySelector('._price').textContent;
-		let unitPrice = parseInt(price.replace(/ /g,''), 10);
-		let priceInt = unitPrice * count;
-		this.sum(true, unitPrice);
+// 	plus() {
+// 		const counter = event.target.closest('._buttons').querySelector('._counter');
+// 		let counterContent = event.target.closest('._buttons').querySelector('._counter').textContent;
+// 		let count = Number(counterContent) + 1;
+// 		counter.innerText = count;
+// 		this.counters();
+
+// 		let price = event.target.closest('.basket__item').querySelector('._price').textContent;
+// 		let unitPrice = parseInt(price.replace(/ /g,''), 10);
+// 		this.sum(true, unitPrice);
+// 	},
+
+// 	// minus(instance) {
+// 	minus() {
+// 		const counter = event.target.closest('._buttons').querySelector('._counter');
+// 		let counterContent = event.target.closest('._buttons').querySelector('._counter').textContent;
+// 		let count = Number(counterContent) - 1;
+
+// 		let price = null;
+// 		let unitPrice = null;
+// 		let priceInt = 0;
+
+// 		if (count !== -1){
+// 			counter.innerText = count;
+// 			price = event.target.closest('.basket__item').querySelector('._price').textContent;
+// 			unitPrice = parseInt(price.replace(/ /g,''), 10);
+// 		}
+
+// 		this.counters();
+
+// 		this.sum(false, unitPrice);
+// 	},
+
+// 	counters() {
+// 		const counters = document.querySelector('._counters');
+// 		const counterAll = document.querySelectorAll('._counter');
+// 		let num = 0;
+// 		counterAll.forEach( function(element) {
+// 			num += Number(element.textContent);
+// 		});
+
+// 		counters.textContent = String(num) + " товаров";
+// 	},
+
+// 	sum(signBool, unitPrice) {
+// 		const sumObj = document.querySelector('._sum');
+// 		let sumInt = 0;
+// 		if (signBool === true){
+// 			sumInt = parseInt(sumObj.textContent.replace(/ /g,''), 10) + unitPrice;
+// 		} else {
+// 			sumInt = parseInt(sumObj.textContent.replace(/ /g,''), 10) - unitPrice;
+// 		}
+// 		sumObj.textContent = String(sumInt).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + " ₽";
+// 	},
+// 	// render(){
+// 	// 	asdas
+// 	// }
+
+
+
+
+// 	change(id, num)
+// 	{
+// 		//
+// 	}
+// }
+// 
+// 
+// 
+
+
+
+o2.basket ={
+	sumContainer: false,
+	countContainer: false,
+	state:{
+		sum: 0,
+		count: 0,
+		items: {
+			1111: {
+				sum: 0,
+				count: 0
+			}
+		},
 	},
 
-	// minus(instance) {
-	minus() {
-		const counter = event.target.closest('._buttons').querySelector('._counter');
-		let counterContent = event.target.closest('._buttons').querySelector('._counter').textContent;
-		let count = Number(counterContent) - 1;
 
-		let price = null;
-		let unitPrice = null;
-		let priceInt = 0;
+	onButton(instance, size, sign) {
+		this.collectData(instance, size, sign);
+	},
 
-		if (count !== -1){
+	collectData(instance, size, sign) {
+		const buttons = instance.closest('._buttons');
+		const counter = buttons.querySelector('._counter');
+		let counterContent = counter.textContent;
+		let count = Number(counterContent);
+
+		let priceSelector = instance.closest('._basket-item').querySelector('._price');
+		let priceStr = priceSelector.textContent;
+		let price = parseInt(priceStr.replace(/ /g,''), 10);
+
+		this.checkSign(sign, instance, size, count, price, counter);
+	},
+
+	checkSign(sign, instance, size, count, price, counter) {
+		if (sign === true) {
+			this.incrementCounter(count, size, price, counter);
+		} else if (count > 0) {
+			this.decrementCounter(count, size, price, counter);
+		}
+	},
+
+	incrementCounter(count, size, price, counter){
+		count += size;
+		this.state.sum += price;
+		this.state.count += size;
+		this.render(count, counter, this.state.sum, this.state.count);
+	},
+
+	decrementCounter(count, size, price, counter){
+		count -= size;
+		this.state.sum -= price;
+		this.state.count -= size;
+		this.render(count, counter, this.state.sum, this.state.count);
+	},
+
+	render(count=null, counter=null, sumInt=null, stateCount) {
+		const sumContainer = document.querySelector('._sum');
+		const countContainer = document.querySelector('._counters');
+		if(counter !== null) {
 			counter.innerText = count;
-			price = event.target.closest('.basket__item').querySelector('._price').textContent;
-			unitPrice = parseInt(price.replace(/ /g,''), 10);
-			priceInt = unitPrice * count;
 		}
-
-		this.counters();
-
-		this.sum(false, unitPrice);
-	},
-
-	counters() {
-		const counters = document.querySelector('._counters');
-		const counterAll = document.querySelectorAll('._counter');
-		let num = 0;
-		counterAll.forEach( function(element) {
-			num += Number(element.textContent);
-		});
-
-		counters.textContent = String(num) + " товаров";
-	},
-
-	sum(signBool, unitPrice) {
-		const sumObj = document.querySelector('._sum');
-		let sumInt = 0;
-		if (signBool === true){
-			sumInt = parseInt(sumObj.textContent.replace(/ /g,''), 10) + unitPrice;
-		} else {
-			sumInt = parseInt(sumObj.textContent.replace(/ /g,''), 10) - unitPrice;
+		if (stateCount > -1) {
+			countContainer.textContent = String(stateCount) + " товаров" ;
 		}
-		sumObj.textContent = String(sumInt).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + " ₽";
+		if (sumInt !==null) {
+			let sum = String(sumInt).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + " ₽";
+			sumContainer.textContent = sum;
+		}
 	},
-	// render(){
-	// 	asdas
-	// }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
