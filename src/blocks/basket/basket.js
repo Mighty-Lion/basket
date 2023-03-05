59c01da76c6104d9c50ac9990273b58daa6320a0
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-03-05 17:44:48 +0300

    dev v0.3

--- src/blocks/basket/basket.js
+++ src/blocks/basket/basket.js
@@ -85,39 +85,173 @@
 
 
 
+// o2.basket ={
+// 	sumContainer: false,
+// 	countContainer: false,
+// 	state:{
+// 		sum: 0,
+// 		count: 0,
+// 		items: {
+// 			1111: {
+// 				sum: 0,
+// 				count: 0
+// 			}
+// 		},
+// 	},
+
+
+// 	onButton(instance, size, sign) {
+// 		this.collectData(instance, size, sign);
+// 	},
+
+// 	collectData(instance, size, sign) {
+// 		const buttons = instance.closest('._buttons');
+// 		const counter = buttons.querySelector('._counter');
+// 		let counterContent = counter.textContent;
+// 		let count = Number(counterContent);
+
+// 		let priceSelector = instance.closest('._basket-item').querySelector('._price');
+// 		let priceStr = priceSelector.textContent;
+// 		let price = parseInt(priceStr.replace(/ /g,''), 10);
+
+// 		this.checkSign(sign, instance, size, count, price, counter);
+// 	},
+
+// 	checkSign(sign, instance, size, count, price, counter) {
+// 		if (sign === true) {
+// 			this.incrementCounter(count, size, counter);
+// 			this.incrementSum(price);
+// 		} else if (count > 0) {
+// 			this.decrementCounter(count, size, counter);
+// 			this.decrementSum(price);
+// 		}
+// 	},
+
+// 	incrementCounter(count, size, counter) {
+// 		count += size;
+// 		this.state.count += size;
+// 		this.render(count, counter, this.state.count);
+// 	},
+
+// 	decrementCounter(count, size, counter) {
+// 		count -= size;
+// 		this.state.count -= size;
+// 		this.render(count, counter, this.state.count);
+// 	},
+
+// 	incrementSum(price) {
+// 		this.state.sum += price;
+// 		this.render( null, null, null, this.state.sum);
+// 	},
+
+// 	decrementSum(price) {
+// 		this.state.sum -= price;
+// 		this.render( null, null, null, this.state.sum);
+// 	},
+
+// 	render(count=null, counter=null, stateCount=null, sumInt=null) {
+// 		const sumContainer = document.querySelector('._sum');
+// 		const countContainer = document.querySelector('._counters');
+// 		if(counter !== null) {
+// 			counter.innerText = count;
+// 		}
+// 		if (stateCount !== null) {
+// 			countContainer.textContent = String(stateCount) + " товаров";
+// 		}
+// 		if (sumInt !==null) {
+// 			let sum = String(sumInt).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + " ₽";
+// 			sumContainer.textContent = sum;
+// 		}
+// 	},
+// }
+
+
+
 o2.basket ={
 	sumContainer: false,
 	countContainer: false,
+	collectFirst: true,
+	itemsAll: null,
 	state:{
 		sum: 0,
 		count: 0,
+		itemsLength: 0,
 		items: {
-			1111: {
-				sum: 0,
-				count: 0
-			}
+			// 1: {
+			// 	price: 0,
+			// 	count: 0
+			// },
+			// 2: {
+			// 	price: 0,
+			// 	count: 0
+			// },
+			// 3: {
+			// 	price: 0,
+			// 	count: 0
+			// },
 		},
 	},
 
 
 	onButton(instance, size, sign) {
-		this.collectData(instance, size, sign);
+		if (this.collectFirst === true) {
+			this.collectData(instance);
+		}
+		this.collectFirst = false;
+
+		// this.isCount(size, sign);
 	},
 
-	collectData(instance, size, sign) {
-		const buttons = instance.closest('._buttons');
-		const counter = buttons.querySelector('._counter');
-		let counterContent = counter.textContent;
-		let count = Number(counterContent);
-
-		let priceSelector = instance.closest('._basket-item').querySelector('._price');
-		let priceStr = priceSelector.textContent;
-		let price = parseInt(priceStr.replace(/ /g,''), 10);
-
-		this.checkSign(sign, instance, size, count, price, counter);
+	checkAmount(instance) {
+		console.log('checkAmount')
+		if (this.collectFirst === true) {
+			this.itemsAll = instance.closest('._basket-list').querySelectorAll('._basket-item');
+			this.itemsAll.forEach( () => ++this.state.itemsLength);
+		}
 	},
 
-	checkSign(sign, instance, size, count, price, counter) {
+	createIdObjects() {
+		console.log('createIdObjects');
+		for (let id = 0; id < this.state.itemsLength; id++) {
+			this.state.items[`${id}`] = {};
+		}
+	},
+
+	createCountersValue(){
+		console.log('createCountersValue');
+		let id = 0;
+		this.itemsAll.forEach( (element) => {
+			const counter = element.querySelector('._counter');
+			let counterContent = counter.textContent;
+			let count = Number(counterContent);
+			this.state.items[`${id}`].count = count ;
+			++id;
+		});
+	},
+
+	createPricesValue(){
+		console.log('createPricesValue');
+		let id = 0;
+		this.itemsAll.forEach( (element) => {
+			const priceSelect = element.querySelector('._price');
+			let priceContent = priceSelect.textContent;
+			let price = parseInt(priceContent.replace(/ /g,''), 10);
+			this.state.items[`${id}`].price = price ;
+			++id;
+		});
+	},
+
+	collectData(instance) {
+		this.checkAmount(instance);
+		this.createIdObjects();
+		this.createCountersValue();
+		this.createPricesValue();
+		console.log('collectData')
+		console.log(this.state.items);
+	},
+
+
+	isCount(size, sign) {
 		if (sign === true) {
 			this.incrementCounter(count, size, counter);
 			this.incrementSum(price);
@@ -148,26 +282,7 @@ o2.basket ={
 		this.state.sum -= price;
 		this.render( null, null, null, this.state.sum);
 	},
-
-	render(count=null, counter=null, stateCount=null, sumInt=null) {
-		const sumContainer = document.querySelector('._sum');
-		const countContainer = document.querySelector('._counters');
-		if(counter !== null) {
-			counter.innerText = count;
-		}
-		if (stateCount !== null) {
-			countContainer.textContent = String(stateCount) + " товаров";
-		}
-		if (sumInt !==null) {
-			let sum = String(sumInt).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + " ₽";
-			sumContainer.textContent = sum;
-		}
-	},
 }
-
-
-
-
 
 
 
