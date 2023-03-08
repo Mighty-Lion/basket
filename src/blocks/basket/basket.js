a83c42a647a9d720f3d574f46c66d105d585ed97
Aslan Abaev <hedgehog5349@gmail.com>
2023-03-08 17:53:28 +0300

    fix basket

--- src/blocks/basket/basket.js
+++ src/blocks/basket/basket.js
@@ -78,7 +78,6 @@ o2.basket ={
 		} else if (targetItem.count > 0){
 			--targetItem.count;
 		}
-
 	},
 
 	renderItems() {
@@ -142,10 +141,10 @@ o2.basket ={
 							</button>
 							<div class="basket__button basket__button--color-inversion _counter">${item.count}</div>
 							<button class="basket__button" onclick="o2.basket.onClick(this, '+1')">
-									<picture>
-										<source srcset="./svg/busketPlus.svg" type="svg/webp">
-										<img class="basket__img" src="./svg/busketPlus.svg" alt="image name">
-									</picture>
+								<picture>
+									<source srcset="./svg/busketPlus.svg" type="svg/webp">
+									<img class="basket__img" src="./svg/busketPlus.svg" alt="image name">
+								</picture>
 							</button>
 						</div>
 					</div>`;
